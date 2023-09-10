d06886c796abce8d68a6af8945e142796f6c78aa
Kopos <carlosh.viveros12@gmail.com>
2023-09-10 11:54:17 +0200

    XTalentoDigital commit

--- page/Home/Home.js
+++ page/Home/Home.js
@@ -2,8 +2,11 @@ import "./Home.css";
 
 const template = () => {
     return `
-    
+   <section class="principal"> 
      <img class="fondo1" src="/images/imagefondo1.jpg" />
+     <div class="text1">
+      <h4>XTalentoDigital ofrece un curso de programación para personas con autismo</h4>
+   </section>
   <section class="info">
    <div class="xtd">
      <h1>¿Qué es el Programa Por Talento Digital?</h1>
@@ -15,9 +18,9 @@ const template = () => {
        <h3>II Convocatoria de becas</h3>
        <p>¡Ya se ha abierto la II Convocatoria de becas de formación individual. </p>
        <p><a class="oportunidad" href="https://portalentodigital.fundaciononce.es/convocatoria-de-becas-de-formacion-individual-por-talento-digital"><u>No pierdas la oportunidad!</u></a></p>
-   </div> 
+    </div> 
 
-     </div>
+   </div>
   </section>
      
 
